58b83045f5336f022fa1f155e4bdf62547332701
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 14:04:07 +0000

    Created method to get calories using xpath

--- XMLLab/xml_parsing.rb
+++ XMLLab/xml_parsing.rb
@@ -16,10 +16,19 @@ class GuiseppesMenu
     get_menu_price.split("£").drop(1)
   end
 
+  def get_xpath_calories
+    @menu.xpath('/breakfast_menu/food/calories')
+  end
+
+
+
 
 end
 
 guiseppes = GuiseppesMenu.new
-puts guiseppes.get_menu_price
-print guiseppes.removePoundFromPrice
-puts guiseppes.removePoundFromPrice
+# puts guiseppes.get_menu_price
+# print guiseppes.removePoundFromPrice
+# puts guiseppes.removePoundFromPrice
+puts guiseppes.get_xpath_calories
+puts guiseppes.get_xpath_calories[0].text.to_i
+puts guiseppes.get_xpath_calories[3].text.to_i
