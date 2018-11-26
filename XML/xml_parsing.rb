2fc0788201b6b4cc4819dba8ef7da10a27478566
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 11:45:10 +0000

    Created method to get food names

--- XML/xml_parsing.rb
+++ XML/xml_parsing.rb
@@ -8,10 +8,13 @@ class GuiseppesMenu
     @menu = Nokogiri::XML(File.open('xml_menu.xml'))
   end
 
-
+  def get_menu_names
+    @menu.search('name')
+  end
 
 
 end
 
 guiseppes = GuiseppesMenu.new
 p guiseppes.menu
+puts guiseppes.get_menu_names
