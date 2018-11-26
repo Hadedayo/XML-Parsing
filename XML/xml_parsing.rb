7a7efe41da9a9d684e0745659a324e60ea096804
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 11:59:11 +0000

    Created method for xpath_calories

--- XML/xml_parsing.rb
+++ XML/xml_parsing.rb
@@ -15,13 +15,25 @@ class GuiseppesMenu
   def get_menu_items
     @menu.search('food')
   end
+
+  def get_xpath_calories
+    @menu.xpath('/breakfast_menu/food/calories')
+  end
+
+
+
+
+
+
+
 end
 
 guiseppes = GuiseppesMenu.new
 # p guiseppes.menu
-puts guiseppes.get_menu_names.last
-
-puts guiseppes.get_menu_names.last
-puts guiseppes.get_menu_names
-puts guiseppes.get_menu_names.length
-puts guiseppes.get_menu_names.last.text
+# puts guiseppes.get_menu_names.last
+#
+# puts guiseppes.get_menu_names.last
+# puts guiseppes.get_menu_names
+# puts guiseppes.get_menu_names.length
+# puts guiseppes.get_menu_names.last.text
+puts guiseppes.get_xpath_calories
