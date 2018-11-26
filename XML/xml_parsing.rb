d5036e3d9d9df6ff22b5c0e7d6242d9220c583be
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 11:37:24 +0000

    Created parse code in def initialize so -->When a file path is pushed in it is parsed. Instantiated the class GuiseppesMenu

--- XML/xml_parsing.rb
+++ XML/xml_parsing.rb
@@ -4,10 +4,14 @@ class GuiseppesMenu
 
   attr_accessor :menu
 
-  def initialize    
+  def initialize
+    @menu = Nokogiri::XML(File.open('xml_menu.xml'))
   end
 
 
 
 
 end
+
+guiseppes = GuiseppesMenu.new
+p guiseppes.menu
