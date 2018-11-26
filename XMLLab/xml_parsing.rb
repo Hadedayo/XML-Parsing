3bf123c710a207337fe2d8977bc6fdecde95f910
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 12:13:36 +0000

    Extracted XML lab folder and all of its contents

---
+++ XMLLab/xml_parsing.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+
+class GuiseppesMenu
+
+  attr_accessor :menu
+
+  def initialize
+    @menu = Nokogiri::XML(File.open('./xml_menu.xml'))
+  end
+
+  # Place your methods here
+
+end
