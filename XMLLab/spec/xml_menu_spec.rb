69774386222b9adf1fc657219fa6dc01fafd3a6d
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 14:26:33 +0000

    Testing menu items with calories over 1000 except for the full breakfast. So far, it fails

--- XMLLab/spec/xml_menu_spec.rb
+++ XMLLab/spec/xml_menu_spec.rb
@@ -14,8 +14,9 @@ describe "XML menu" do
   end
 
   it "should have no item with calories over 1000 except for the full breakfast" do
-    # Consider children element methods - Remember to step through the data and print out to the command line if you need it
-    pending
+    @xml_menu.get_xpath_calories.each do
+      expect(@xml_menu.calories_array). to be < 1000
+    end
   end
 
   it "should have all waffle dishes stating you get two waffles" do
